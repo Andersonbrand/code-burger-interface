6898d41e85c43e734fe834a238cba0542b652d90
Andersontbrand <andersonbteixeira@outlook.com>
2023-12-27 18:14:47 -0300

    update-tela-home-27-12

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
+import CategoryCarousel from '../../components/CategoryCarousel'
+
+import HomeLogo from '../../assets/burger -logo.svg'
+import { Container, HomeImg } from './styles'
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <Container>
+            <HomeImg src={HomeLogo} alt='logo da home' />
+            <CategoryCarousel />
+        </Container>
+    )
 }
 
 export default Home
